a5b3f574dcd1ee910975a4d752bca0420f3879bd
campossalex <32500181+campossalex@users.noreply.github.com>
2025-08-19 11:20:43 +0200

    Update kafka.py

--- datagen/config/kafka.py
+++ datagen/config/kafka.py
@@ -3,15 +3,11 @@ import configparser
 
 def get_configs():
     config = configparser.ConfigParser()
-    config.read("configuration/configuration.ini")
+    config.read("/root/ververica-platform-playground/datagen/configuration/configuration.ini")
 
     bootstrap_servers = config["KAFKA"]["bootstrap_servers"]
-    #auth_method = config["KAFKA"]["auth_method"]
-    #sasl_username = config["KAFKA"]["sasl_username"]
-    #sasl_password = config["KAFKA"]["sasl_password"]
 
     configs = {"bootstrap_servers": bootstrap_servers}
-
 
     print("configs: {0}".format(str(configs)))
 
